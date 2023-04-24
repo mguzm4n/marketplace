81382d42178e2cdac9d32b56032c29a83f07e072
Marcelo <marcelo.guzman.g@usach.cl>
2023-04-24 02:16:52 -0400

    add sql to update reaction

--- market-back/src/main/java/com/example/authjava/Repositories/MessageRepository.java
+++ market-back/src/main/java/com/example/authjava/Repositories/MessageRepository.java
@@ -5,6 +5,7 @@ import com.example.authjava.Data.DTOs.MessageProjection;
 import com.example.authjava.Data.Models.MessageModel;
 import org.aspectj.bridge.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
@@ -23,4 +24,11 @@ public interface MessageRepository extends JpaRepository<MessageModel, Long> {
             "ORDER BY m.sent_at;"
     )
     List<MessageProjection> findAllByProductIdGrouped(@Param("productId") Long productId);
+
+    @Query(
+        nativeQuery = true,
+        value = "UPDATE messages m SET reaction_id=:reactionId WHERE m.id=:messageId"
+    )
+    @Modifying
+    int updateMessageReaction(@Param("messageId") Long messageId, @Param("reactionId") Long reactionId);
 }
